92356640d798755ff0c83fcf71e20e1fe6bd0bf7
unknown <alhanan055224@gmail.com>
2024-09-20 01:38:01 +0300

    An update of the functions and an arrangement of the functions that will be readable and clear

--- TechWorld/JS/genericHtml.js
+++ TechWorld/JS/genericHtml.js
@@ -1,8 +1,176 @@
 /**
- * create the box of all element in the site
- * @param {json} json
+ *build html of the top line for inner page in site
  */
-export function createBox(_json) {
+function buildTopLine() {
+  return `
+         <div class="share col-lg-4">
+            <button
+              class="share-btn text-center"
+              onclick="displayElement('linkTechWorld',-1)"
+            >
+              Share
+            </button>
+
+            <div class="text-center" id="linkTechWorld">
+              copy link <a href="#">TechWorld</a>
+            </div>
+          </div>
+
+          <div class="col-lg-8 row">
+            <div class="col-lg-6"></div>
+            <div class="col-lg-3 text-end sort">
+              <button class="sort-btn" onclick="displayElement('sortList',0)">
+                sort
+                <!-- arrow down -->
+                <span class="arrow_down"> </span>
+
+                <!-- arrow up -->
+                <span class="arrow_up" style="display: none"> </span>
+              </button>
+
+              <div
+                id="sortList"
+                class="text-end sort_list"
+                style="background: white"
+              >
+                <button onclick="lowToHigh(smartphones_arr,'Smartphone')">מחיר נמוך לגבוה</button>
+                <button onclick="highToLow(smartphones_arr,'Smartphone')">מחיר גבוה לנמוך</button>
+                <button onclick="popular(smartphones_arr,'Smartphone')">המוצרים הכי חמים</button>
+              </div>
+            </div>
+
+            <div class="col-lg-3 text-end filter">
+              <button
+                class="filter-btn"
+                onclick="displayElement('listFiltering',1)"
+              >
+                Filtering
+                <!-- arrow down -->
+                <span class="arrow_down"> </span>
+
+                <!-- arrow up -->
+                <span class="arrow_up" style="display: none"> </span>
+              </button>
+
+              <div
+                id="listFiltering"
+                class="list_filtering"
+                style="background: white"
+              >
+                <div>
+                  <button onclick="displayElement('listPrice',2)">
+                    Filtering by price
+                    <!-- arrow down -->
+                    <span class="arrow_down"> </span>
+
+                    <!-- arrow up -->
+                    <span class="arrow_up" style="display: none"> </span>
+                  </button>
+                  <div id="listPrice" style="display: none">
+                    <div>
+                      <input
+                        id="toPrice"
+                        class="ml-5"
+                        type="number"
+                        placeholder="5,429"
+                        width="20px"
+                      />
+
+                      <input
+                        id="fromPrice"
+                        type="number"
+                        placeholder="389"
+                        width="20px"
+                      />
+                    </div>
+
+                    <div class="position-relative mt-2" style="height: 40px">
+                      <input
+                        type="range"
+                        class="form-range slider position-absolute top-0 start-0"
+                        min="389"
+                        max="5429"
+                        id="priceRange"
+                        value="389"
+                        style="width: 100%"
+                      />
+                      <input
+                        type="range"
+                        class="form-range slider position-absolute top-0 start-0"
+                        min="389"
+                        max="5429"
+                        id="priceRangeMax"
+                        value="5429"
+                        style="width: 100%"
+                      />
+                    </div>
+
+                    <div class="filter-in">
+                      <button onclick="filter(smartphones_arr,'Smartphone')" class="mt-2">Filter</button>
+                    </div>
+                  </div>
+
+                  <button
+                    class="mt-2"
+                    onclick="displayElement('listCompany',3)"
+                  >
+                    Company
+                    <!-- arrow down -->
+                    <span class="arrow_down"> </span>
+
+                    <!-- arrow up -->
+                    <span class="arrow_up" style="display: none"> </span>
+                  </button>
+                  <div
+                    id="listCompany"
+                    class="text-start m-1"
+                    style="display: none"
+                    id="Company"
+                  >
+                    <input type="checkbox" />
+                    Apple
+                    <br />
+                    <input type="checkbox" />
+                    Samsung
+                    <br />
+                    <input type="checkbox" />
+                    Xiaomi
+                  </div>
+
+                  <button class="mt-2" onclick="displayElement('listColor',4)">
+                    By Color
+                    <!-- arrow down -->
+                    <span class="arrow_down"> </span>
+
+                    <!-- arrow up -->
+                    <span class="arrow_up" style="display: none"> </span>
+                  </button>
+                  <div
+                    class="text-start m-1"
+                    id="listColor"
+                    style="display: none"
+                  >
+                    <input type="checkbox" />
+                    Black
+                    <br />
+                    <input type="checkbox" />
+                    White
+                    <br />
+                    <input type="checkbox" />
+                    Blue
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  `;
+}
+
+/**
+ * build html of the box of all element in the site
+ * @param {object} _json data of page
+ */
+function BuildBox(_json) {
   return `
   <div class="picBox mx-auto mt-4">
   <img class="image" src="../Images/${_json.image}" alt="${_json.name}" >
@@ -34,7 +202,7 @@ export function createBox(_json) {
        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
           <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
         </svg>
-    </button>
+    </button>
  
      <h3 id="price"  class="mb-0" style="margin-top:0;">${_json.price}</h3>
   </div>
