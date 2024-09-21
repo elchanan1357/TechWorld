4070204dd0adfadb41485dcae061998487a3e872
YehielShamir <y.shamir990@gmail.com>
2024-09-21 17:38:28 +0300

    navbar

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,15 @@
 const linkConfigs = [
-  { href: "Sales.html", img: "../Images/index/sale.png" },
+  { href: 'Sales.html', img: '../Images/index/sale.png' },
   {
-    href: "https://www.calcalist.co.il/calcalistech/category/3778",
-    img: "../Images/index/whats-new.jpg",
+    href: 'https://www.calcalist.co.il/calcalistech/category/3778',
+    img: '../Images/index/whats-new.jpg',
   },
 ];
 let currentIndex = 0;
 
 function replaceLink() {
-  const link = document.getElementById("dynamicLink");
-  const img = document.getElementById("dynamicImage");
+  const link = document.getElementById('dynamicLink');
+  const img = document.getElementById('dynamicImage');
 
   currentIndex = (currentIndex + 1) % linkConfigs.length; // Cycle through the links
   link.href = linkConfigs[currentIndex].href;
