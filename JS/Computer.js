039ced337764c320bda22b082bf1063fb105ff43
unknown <alhanan055224@gmail.com>
2024-09-22 17:22:09 +0300

    create the computer page

--- JS/Computer.js
+++ JS/Computer.js
@@ -1,1 +1,42 @@
+window.onload = function () {
+  createTopLine();
 
+  print(computers_arr, "Computer"); //create element in page
+
+  document.getElementById("listColor").innerHTML = listOfColor();
+  document.getElementById("listCompany").innerHTML = listOfCompany();
+
+  for (let i = 0; i < 5; i++) createArrow("arrow_up", "arrow_down", i);
+};
+
+/**
+ * top line in button company
+ * create list of options
+ */
+function listOfCompany() {
+  return `    <input onchange="filterByCompany(computers_arr,'Apple','Computer',0)" class="checkBox" type="checkbox" />
+                      Apple
+                      <br />
+                      <input onchange="filterByCompany(computers_arr,'Samsung','Computer',1)" class="checkBox" type="checkbox" />
+                      Samsung
+                      <br />
+                      <input onchange="filterByCompany(computers_arr,'Xiaomi','Computer',2)" class="checkBox" type="checkbox" />
+                      Xiaomi`;
+}
+
+/**
+ * top line in button color
+ * create list of options
+ */
+function listOfColor() {
+  return `
+                    <input onchange="filterByColor(computers_arr,'Black','Computer',3)"  class="checkBox" type="checkbox" />
+                      Black
+                      <br />
+                      <input onchange="filterByColor(computers_arr,'White','Computer',4)" class="checkBox" type="checkbox" />
+                      White
+                      <br />
+                      <input onchange="filterByColor(computers_arr,'Silver','Computer',5)" class="checkBox" type="checkbox" />
+                      Silver
+    `;
+}
--- JS/index.js
+++ JS/index.js
@@ -1,48 +1,36 @@
 const linkConfigs = [
-  { href: 'Sales.html', img: '../Images/index/sale.png' },
+  { href: "Sales.html", img: "../Images/index/sale.png" },
   {
-    href: 'https://www.calcalist.co.il/calcalistech/category/3778',
-    img: '../Images/index/whats-new.jpg',
+    href: "https://www.calcalist.co.il/calcalistech/category/3778",
+    img: "../Images/index/whats-new.jpg",
   },
 ];
+
 let currentIndex = 0;
-
-// function replaceLink() {
-//   const link = document.getElementById('dynamicLink');
-//   const img = document.getElementById('dynamicImage');
-
-//   currentIndex = (currentIndex + 1) % linkConfigs.length; // Cycle through the links
-//   link.href = linkConfigs[currentIndex].href;
-//   img.src = linkConfigs[currentIndex].img;
-// }
-
-// // Change link every 2.5 seconds
-// setInterval(replaceLink, 2500);
 
 // Function to get random items from a category array
 function getRandomProducts(categoryArray, numberOfItems) {
   const shuffledArray = [...categoryArray].sort(() => 0.5 - Math.random());
-  console.log('first log: ' + shuffledArray)
-  return shuffledArray.slice(0, numberOfItems); 
+  console.log("first log: " + shuffledArray);
+  return shuffledArray.slice(0, numberOfItems);
 }
 
 // Function to display random products on the homepage
 function displayRandomProducts() {
-
   // Get random products from each category
   const randomSmartphones = getRandomProducts(allProducts.smartphones_arr, 4);
-  const randomTablets = getRandomProducts(allProducts.tablets_arr, 3);        
-  const randomComputers = getRandomProducts(allProducts.computers_arr, 3);    
-  const randomProps = getRandomProducts(allProducts.props_arr, 4);            
+  const randomTablets = getRandomProducts(allProducts.tablets_arr, 3);
+  const randomComputers = getRandomProducts(allProducts.computers_arr, 3);
+  const randomProps = getRandomProducts(allProducts.props_arr, 4);
 
   // Display products on the homepage using the addToBox function
-  randomSmartphones.forEach(product => addToBox(product, 'Smartphones'));
-  randomTablets.forEach(product => addToBox(product, 'Tablets'));
-  randomComputers.forEach(product => addToBox(product, 'Computers'));
-  randomProps.forEach(product => addToBox(product, 'Props'));
+  randomSmartphones.forEach((product) => addToBox(product, "Smartphones"));
+  randomTablets.forEach((product) => addToBox(product, "Tablets"));
+  randomComputers.forEach((product) => addToBox(product, "Computers"));
+  randomProps.forEach((product) => addToBox(product, "Props"));
 }
 
 // Call the function to display products when the page loads
-window.onload = function() {
+window.onload = function () {
   displayRandomProducts();
 };
