587abaca752e60f619bc147a60c80d5992def73e
YehielShamir <y.shamir990@gmail.com>
2024-09-24 01:02:51 +0300

    increase to 4 items each category

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,8 @@
 const linkConfigs = [
-  { href: "Sales.html", img: "../Images/index/sale.png" },
+  { href: 'Sales.html', img: '../Images/index/sale.png' },
   {
-    href: "https://www.calcalist.co.il/calcalistech/category/3778",
-    img: "../Images/index/whats-new.jpg",
+    href: 'https://www.calcalist.co.il/calcalistech/category/3778',
+    img: '../Images/index/whats-new.jpg',
   },
 ];
 
@@ -11,7 +11,6 @@ let currentIndex = 0;
 // Function to get random items from a category array
 function getRandomProducts(categoryArray, numberOfItems) {
   const shuffledArray = [...categoryArray].sort(() => 0.5 - Math.random());
-  console.log("first log: " + shuffledArray);
   return shuffledArray.slice(0, numberOfItems);
 }
 
@@ -19,15 +18,15 @@ function getRandomProducts(categoryArray, numberOfItems) {
 function displayRandomProducts() {
   // Get random products from each category
   const randomSmartphones = getRandomProducts(allProducts.smartphones_arr, 4);
-  const randomTablets = getRandomProducts(allProducts.tablets_arr, 3);
-  const randomComputers = getRandomProducts(allProducts.computers_arr, 3);
+  const randomTablets = getRandomProducts(allProducts.tablets_arr, 4);
+  const randomComputers = getRandomProducts(allProducts.computers_arr, 4);
   const randomProps = getRandomProducts(allProducts.props_arr, 4);
 
   // Display products on the homepage using the addToBox function
-  randomSmartphones.forEach((product) => addToBox(product, "Smartphones"));
-  randomTablets.forEach((product) => addToBox(product, "Tablets"));
-  randomComputers.forEach((product) => addToBox(product, "Computers"));
-  randomProps.forEach((product) => addToBox(product, "Props"));
+  randomSmartphones.forEach((product) => addToBox(product, 'Smartphones'));
+  randomTablets.forEach((product) => addToBox(product, 'Tablets'));
+  randomComputers.forEach((product) => addToBox(product, 'Computers'));
+  randomProps.forEach((product) => addToBox(product, 'Props'));
 }
 
 // Call the function to display products when the page loads
